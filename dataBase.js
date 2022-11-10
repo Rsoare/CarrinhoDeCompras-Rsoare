57b949aaceebf5971e3d1dbfdd09da3b0b380581
Rihan soares <rihan145@gmail.com>
2022-11-10 18:07:22 -0300

    Entrega Concluida

---
+++ dataBase.js
@@ -0,0 +1,27 @@
+const productsCart = [
+    {
+        id: 1,
+        name: "laranja",
+        price: 365.33
+    },
+    {
+        id: 2,
+        name: "pera",
+        price: 547.71
+    },
+    {
+        id: 3,
+        name: "uva",
+        price: 369.59
+    },
+    {
+        id: 4,
+        name: "limão",
+        price: 471.10
+    },
+    {
+        id: 5,
+        name: "melancia",
+        price: 254.78
+    }
+]
--- index.js
+++ index.js
@@ -1,7 +1,80 @@
-const productsCart = [
-    {
-        id: 1,
-        name: "",
-        price: 0.00
+let body = document.querySelector("body")
+body.insertAdjacentHTML("afterbegin", "<header class=header-edit><h1>Virtual Market</h1></header>")
+let soma = 0
+
+let main = document.createElement("main")
+main.classList.add("main-edit")
+body.appendChild(main)
+
+let section = document.createElement("section")
+section.classList.add("secaoProdutos")
+main.appendChild(section)
+
+let div = document.createElement("div")
+div.classList.add("divProdutos")
+section.appendChild(div)
+
+let p1 = document.createElement("p")
+p1.innerHTML = "item"
+p1.classList.add("paragrafoProdutos")
+div.appendChild(p1)
+
+let p2 = document.createElement("p")
+p2.innerHTML = "preço"
+p2.classList.add("paragrafoProdutos")
+div.appendChild(p2)
+
+let productList = document.createElement('ul')
+productList.classList.add("listaProdutos")
+section.appendChild(productList)
+
+let section2 = document.createElement("section")
+section2.classList.add("secaoFinal")
+main.appendChild(section2)
+
+let div2 = document.createElement("div")
+div2.classList.add("divFinal")
+section2.appendChild(div2)
+
+let pTotal = document.createElement("p")
+pTotal.innerHTML = "Total"
+pTotal.classList.add("valorTotal")
+div2.appendChild(pTotal)
+
+let ButtonEnd = document.createElement("button")
+ButtonEnd.innerHTML = "Finalizar Comprar"
+ButtonEnd.classList.add("botao")
+section2.appendChild(ButtonEnd)
+
+
+function listaDeProdutos(arrObj) {
+    for (let i = 0; i < arrObj.length; i++) {
+        let productDetails = document.createElement("li")
+        productDetails.classList.add("produto")
+        productList.appendChild(productDetails)
+
+        let nomeProduto = document.createElement("p")
+        nomeProduto.classList.add("nome")
+        nomeProduto.innerHTML = `${arrObj[i].name}`
+        productDetails.appendChild(nomeProduto)
+
+        let precoProduto = document.createElement("span")
+        precoProduto.classList.add("preco")
+        precoProduto.innerHTML = `R$ ${arrObj[i].price.toFixed(2)}`
+        productDetails.appendChild(precoProduto)
     }
-]
+}
+listaDeProdutos(productsCart)
+
+function somaProdutos(arrObj) {
+    for (let i = 0; i < arrObj.length; i++) {
+        soma += arrObj[i].price
+    } return soma
+}
+somaProdutos(productsCart)
+
+let spanTotal = document.createElement("span")
+spanTotal.innerHTML = `R$ ${soma.toFixed(2)}`
+spanTotal.classList.add("valorTotal")
+div2.appendChild(spanTotal)
+
